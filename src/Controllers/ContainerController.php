024ada1b89b615e1752ffaed2548e9d235606491
Vaidas <vaidasl07@gmail.com>
2023-10-24 01:04:00 +0300

    Added first version of calculation algorithm

--- src/Controllers/ContainerController.php
+++ src/Controllers/ContainerController.php
@@ -34,9 +34,86 @@ class ContainerController extends Controller {
     {
         $transports = $_POST['transports'];
         $containers = $_POST['containers'];
+        $amount = '';
 
         // Deserialize the data back to arrays
         $transports = json_decode($transports, true);
         $containers = json_decode($containers, true);
+
+        foreach ($transports as $transport)
+        {
+            $amount = '';
+            foreach ($transport['packages'] as &$package)
+            {
+                while($package['amount'] > 0)
+                {
+                    $amount = $this->calculatePackageInContainer($containers, $package, $amount);
+                    if($amount['amount_left'] == 0)
+                    {
+                        $package['amount'] = $amount['amount_left'];
+                    }
+                    else
+                    {
+                        $package['amount'] = $package['amount'] -  $amount['amount_left'];
+                    }
+                }
+            }
+        }
+    }
+
+    public function calculatePackageInContainer($containers, $package, $amount)
+    {
+        $amount_left = [];
+        if(!empty($amount) && $amount['amount_left'] > 0)
+        {
+            $lowest_amount = $this->fillRestContainer($amount, $package);
+            return $lowest_amount;
+        }
+        foreach ($containers as $key => $container)
+        {
+            $fill_length = floor($container['length'] / $package['length']);
+            $fill_height = floor($container['height'] / $package['height']);
+            $fill_width = floor($container['width'] / $package['width']);
+            $max_amount = $fill_length * $fill_height * $fill_width;
+            $amount_left[$key]['amount_left'] = $max_amount - $package['amount'];
+
+            $amount_left[$key]['container_place_left'] = ($container['width'] * $container['height'] * $container['length']) - ($package['length'] * $package['height'] * $package['width'] * $package['amount']);
+        }
+
+        $lowest_amount = $this->lowestAmount($amount_left);
+
+        return $lowest_amount;
+    }
+
+    public function lowestAmount($amount_left)
+    {
+        $lowest_amount = min($amount_left);
+        foreach ($amount_left as $key => $value) {
+            if ($value['amount_left'] === $lowest_amount['amount_left']) {
+                $lowest_amount['container_key'] = $key;
+                break;
+            }
+        }
+
+        return $lowest_amount;
+    }
+
+    public function fillRestContainer($amount, $package)
+    {
+        $max_amount = floor($amount['container_place_left'] / $package['length'] / $package['height'] / $package['width']);
+        if($max_amount < $package['amount'])
+        {
+            $amount_left[$amount['container_key']]['amount_left'] = 0;
+            $amount_left[$amount['container_key']]['container_place_left'] = 0;
+
+            $package['amount'] = $package['amount'] - $max_amount;
+        }
+        else
+        {
+            $package['amount'] = 0;
+            $amount_left[$amount['container_key']]['container_place_left'] = $amount['container_place_left'] - ($package['length'] * $package['height'] * $package['width'] * $package['amount']);
+            $amount_left[$amount['container_key']]['amount_left'] = $max_amount - $package['amount'];
+        }
+        return $this->lowestAmount($amount_left);
     }
 }
